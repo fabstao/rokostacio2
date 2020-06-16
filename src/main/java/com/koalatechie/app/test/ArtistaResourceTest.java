8317833429f304a410e0c5b3c9cc0edaa92e65d2
fabstao <fabian@nuo.com.mx>
2020-06-16 09:57:17 -0500

    Working on tests

---
+++ src/main/java/com/koalatechie/app/test/ArtistaResourceTest.java
@@ -0,0 +1,13 @@
+package com.koalatechie.app.test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+public class ArtistaResourceTest {
+
+}
